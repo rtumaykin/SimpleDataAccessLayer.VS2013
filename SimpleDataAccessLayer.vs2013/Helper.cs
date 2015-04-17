295d68e400be85a09ea7bd1557bfdbcf4d8c20b3
Roman Tumaykin <roman@tumaykin.com>
2015-04-17 02:20:01 -0700

    Work in progress.

--- SimpleDataAccessLayer.vs2013/Helper.cs
+++ SimpleDataAccessLayer.vs2013/Helper.cs
@@ -178,16 +178,18 @@ namespace SimpleDataAccessLayer_vs2013
 						    var schemaName = reader.GetSqlString(6).Value;
 						    var isTableType = reader.GetSqlBoolean(7).Value;
 
-                            // make sure that if it is a custom type and we can't find base type, we use fully qualified name
-						    sqlTypeName = schemaName == "sys" ? schemaName : string.Format("[{0}].[{1}]", schemaName, sqlTypeName);
-
-							var clrTypeName = Tools.ClrTypeName(sqlTypeName);
+							var clrTypeName = schemaName == "sys" ? 
+                                Tools.ClrTypeName(sqlTypeName) : 
+                                string.Format("{0}.{1}", Tools.CleanName(schemaName), Tools.CleanName(sqlTypeName));
 
 						    if (!string.IsNullOrWhiteSpace(clrTypeName) || isTableType)
 						    {
-                                retValue.Add(new ProcedureParameter(parameterName, maxByteLength, precision, scale, isOutputParameter, sqlTypeName));
-                            }
-
+						        retValue.Add(new ProcedureParameter(parameterName, maxByteLength, precision, scale, isOutputParameter,
+						            schemaName == "sys"
+						                ? sqlTypeName
+						                : string.Format("{0}.{1}", Tools.QuoteName(schemaName), Tools.QuoteName(sqlTypeName)), 
+                                    isTableType));
+						    }
 						}
 					}
 				}
@@ -337,5 +339,149 @@ namespace SimpleDataAccessLayer_vs2013
 			}
 			return retValue;
 		}
+
+	    public string GetCodeForTableValuedParameters()
+	    {
+            var tableTypes = GetTableTypes();
+	        
+            if (tableTypes.Count == 0)
+	            return "";
+            
+            var code = "";
+
+	        string nameSpace = null;
+	        foreach (var tableType in tableTypes)
+	        {
+	            if (tableType.SchemaName != nameSpace)
+	            {
+                    // first namespace
+	                if (string.IsNullOrWhiteSpace(nameSpace))
+	                {
+	                    code += "}\r";
+	                }
+	                nameSpace = tableType.SchemaName;
+	                code += string.Format("namespace {0}.{1}.{2}\r{{\r", Config.Namespace, "TableVariables",
+	                    Tools.CleanName(nameSpace));
+	            }
+
+                var columns = GetTableTypeColumns(tableType);
+
+	            code += string.Format("public class {0}Row {{\r{1}\r}}", Tools.CleanName(tableType.Name), GetCodeForTableTypeColumns(tableType, columns));
+	            code +=
+	                string.Format(
+	                    "public class {0} : List<{0}Row> {{public {0} (IEnumerable<{0}Row> collection) : base(collection){{}}\rinternal DataTable GetDataTable() {{{1}}}\r}}",
+	                    Tools.CleanName(tableType.Name), GetCodeForDataTableConversion(tableType, columns));
+	        }
+
+	        code += "}\r";
+
+	        return code;
+	    }
+
+	    private string GetCodeForDataTableConversion(TableType tableType, IList<TableTypeColumn> columns)
+	    {
+            var code = "var dt = new DataTable();\r";
+	        code += string.Join("\r",
+	            columns.Select(
+	                column => string.Format("dt.Columns.Add(\"{0}\", typeof({1}));", column.ColumnName, column.ClrTypeName)));
+
+            // vcreate a foreach loop
+
+	        code += "return dt;";
+	        return code;
+	    }
+
+	    private string GetCodeForTableTypeColumns(TableType tableType, IList<TableTypeColumn> columns)
+	    {
+            var code = string.Join("\r", columns.Select(
+	            column => string.Format("public global::{0} {1} {{ get; private set; }}\r", column.ClrTypeName, Tools.CleanName(column.ColumnName))));
+
+	        code += string.Format("public {0}({1}) {{{2}}}\r", Tools.CleanName(tableType.Name),
+                 string.Join(", ", columns.Select(column=>string.Format("global::{0} {1}", column.ClrTypeName, column.ColumnName.Substring(0, 1).ToLower() + column.ColumnName.Substring(1)))),
+                 string.Join("\r", columns.Select(column => string.Format("this.{0} = {1};", column.ColumnName, column.ColumnName.Substring(0, 1).ToLower() + column.ColumnName.Substring(1))))
+                );
+	        return code;
+	    }
+
+	    private IList<TableTypeColumn> GetTableTypeColumns(TableType tableType)
+	    {
+            var retValue = new List<TableTypeColumn>();
+
+            using (var conn = new SqlConnection(DesignerConnectionString))
+            {
+                conn.Open();
+
+                using (var cmd = conn.CreateCommand())
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.CommandText = "sp_executesql";
+
+                    const string stmt =
+                        "SELECT "+
+	                        "[c].[name] AS ColumnName, "+
+	                        "SCHEMA_NAME(ISNULL([st].[schema_id], [ut].[schema_id])) AS SchemaName, " +
+	                        "ISNULL([st].[name], [ut].[name]) AS DataTypeName "+
+                        "FROM [sys].[table_types] tt " +
+	                        "INNER JOIN [sys].[columns] c "+
+		                        "ON	[c].[object_id] = [tt].[type_table_object_id] "+
+	                        "INNER JOIN [sys].[types] ut "+
+		                        "ON	[ut].[user_type_id] = [c].[user_type_id] "+
+	                        "LEFT OUTER JOIN [sys].[types] st "+
+		                        "ON	[st].[user_type_id] = [c].[system_type_id]"+
+                        "WHERE "+ 
+	                        "[tt].[schema_id] = SCHEMA_ID(@SchemaName) "+
+	                        "AND [tt].[name] = @ObjectName;";
+
+                    cmd.Parameters.AddWithValue("@stmt", stmt);
+                    cmd.Parameters.AddWithValue("@params", "@SchemaName sysname, @ObjectName sysname");
+                    cmd.Parameters.AddWithValue("@SchemaName", tableType.SchemaName);
+                    cmd.Parameters.AddWithValue("@ObjectName", tableType.Name);
+
+
+                    using (var reader = cmd.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            var schemaName = reader.GetSqlString(1).Value;
+                            var sqlTypeName = reader.GetSqlString(2).Value;
+                            var clrTypeName = schemaName == "sys" ? Tools.ClrTypeName(sqlTypeName) : "System.Object";
+
+                            retValue.Add(new TableTypeColumn(reader.GetSqlString(0).Value, clrTypeName));
+                        }
+                    }
+                }
+            }
+            return retValue;
+        }
+
+	    private IList<TableType> GetTableTypes()
+	    {
+            var retValue = new List<TableType>();
+
+            using (var conn = new SqlConnection(DesignerConnectionString))
+            {
+                conn.Open();
+
+                using (var cmd = conn.CreateCommand())
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.CommandText = "sp_executesql";
+
+                    const string stmt =
+                        "SELECT SCHEMA_NAME([schema_id]) AS [SchemaName], [name] AS [ObjectName] FROM [sys].[table_types];";
+
+                    cmd.Parameters.AddWithValue("@stmt", stmt);
+
+                    using (var reader = cmd.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            retValue.Add(new TableType(reader.GetSqlString(0).Value, reader.GetSqlString(1).Value));
+                        }
+                    }
+                }
+            }
+            return retValue;
+	    }
 	}
 }
---
+++ SimpleDataAccessLayer.vs2013/TableTypeDescriptor.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleDataAccessLayer_vs2013
+{
+    public class TableTypeColumn
+    {
+        public string ColumnName { get; private set; }
+
+        public string ClrTypeName { get; private set; }
+
+        public TableTypeColumn(string columnName, string clrTypeName)
+        {
+            ColumnName = columnName;
+            if ("System.Int64 System.Boolean System.DateTime System.DateTimeOffset System.Decimal System.Double Microsoft.SqlServer.Types.SqlHierarchyId System.Int32 System.Single System.Int16 System.TimeSpan System.Byte System.Guid".Split(' ').Contains(clrTypeName))
+            {
+                clrTypeName += "?";
+            }
+            ClrTypeName = clrTypeName;
+        }
+    }
+
+    public class TableType
+    {
+        public string SchemaName { get; private set; }
+        public string Name { get; private set; }
+
+        public TableType(string schemaName, string name)
+        {
+            SchemaName = schemaName;
+            Name = name;
+        }
+    }
+}
